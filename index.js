bc74319e17c310aa421c647388110d4814f7b665
Racquel Glickman <racquel.glickman@gmail.com>
2023-03-11 22:44:58 -0500

    completes looping lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+
+function writeCards(names, event){
+    const messages = []
+    for (let i = 0; i < names.length; i++){
+        messages[i] = `Thank you, ${names[i]}, for the wonderful ${event} gift!`
+    }
+    console.log(messages);
+    return messages;
+}
+
+writeCards(["Racquel", "Gil", "Lilly"], "Purim");
+
+function countDown(integer){
+    while (integer >= 0){
+        console.log(integer);
+        integer--;
+    }
+}
+
+countDown(13);
